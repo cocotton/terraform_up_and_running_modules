67931baa23360769b588ae5922a5e4226dfcd17e
Cocotton <julien.parent.trudeau@gmail.com>
2018-06-12 19:27:05 -0400

    Create the low_cpu_credit_balance alert for "t" type instances

--- aws/services/terraform-up-and-running/main.tf
+++ aws/services/terraform-up-and-running/main.tf
@@ -151,3 +151,22 @@ resource "aws_cloudwatch_metric_alarm" "high_cpu_utilization" {
   threshold           = 90
   unit                = "Percent"
 }
+
+resource "aws_cloudwatch_metric_alarm" "low_cpu_credit_balance" {
+  count = "${format("%.1s", var.instance_type) == "t" ? 1 : 0}"
+
+  alarm_name  = "${var.cluster_name}-low-cpu-credit-balance"
+  namespace   = "AWS/EC2"
+  metric_name = "CPUCreditBalance"
+
+  dimensions = {
+    AutoScalingGroupName = "${aws_autoscaling_group.example_instance.name}"
+  }
+
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  period              = 300
+  statistic           = "Minimum"
+  threshold           = 10
+  unit                = "Count"
+}
